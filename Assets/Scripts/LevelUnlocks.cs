49d721e0b8411ee2a1fb659c7d0cd2fe9f694615
Joshua Kim-Pearson <jkimpear@ucsc.edu>
2026-06-09 23:51:13 -0700

    hub progression

---
+++ Assets/Scripts/LevelUnlocks.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LevelUnlocks : MonoBehaviour
+{
+    [SerializeField] private GameObject gameObjectToUnlock;
+    [SerializeField] private int levelIndexToUnlock;
+    [SerializeField] private bool activityUponUnlock = false;
+    [SerializeField] private bool isDoor = false;
+
+    private void Start()
+    {
+        if (!isDoor)
+        {
+            if (GameManager.Instance.HighestLevel >= levelIndexToUnlock)
+            {
+                gameObjectToUnlock.SetActive(activityUponUnlock);
+            }
+            else
+            {
+                gameObjectToUnlock.SetActive(!activityUponUnlock);
+            }
+        }
+        if (isDoor)
+        {
+            if (GameManager.Instance.HighestLevel >= levelIndexToUnlock)
+            {
+                gameObjectToUnlock.GetComponent<Door>().enabled = true;
+            }
+            else
+            {
+                gameObjectToUnlock.GetComponent<Door>().enabled = false;
+            }
+        }
+    }
+}
